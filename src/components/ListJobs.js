aedb23c11bfb05aa1d601fc167819e7f2a2b8ad1
Raheem Ali <127837308+raheem-a1i@users.noreply.github.com>
2025-11-08 23:26:41 +0000

    Update

--- src/components/ListJobs.js
+++ src/components/ListJobs.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import UpdateJobs from "./UpdateJobs";
 import StatusCounts from "./StatusCounts";
 import { decodeHTMLEntities } from "./Helper";
