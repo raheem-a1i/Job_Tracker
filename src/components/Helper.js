92be486a1f7727e3a1654e7ed108a44ee46191c3
Raheem Ali <127837308+raheem-a1i@users.noreply.github.com>
2025-11-08 23:18:43 +0000

    remove

--- src/components/Helper.js
+++
@@ -1,6 +0,0 @@
-export function decodeHTMLEntities(text) {
-     const element = document.createElement("div");
-     element.innerHTML = text;
-     return element.textContent || element.innerText;
-}
-
